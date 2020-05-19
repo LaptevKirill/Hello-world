3c484991ec250978ab4a17fd9d2116479312fb1b
Laptev Kirill <Badman_ne@mail.ru>
2020-05-19 22:16:03 +0400

    Hello World app

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String hello = "Hello";
-        String world = "World";
+	String hello = "Hello";
+	String world = "world!";
         System.out.println(hello + " " + world + "!");
+
     }
 }
